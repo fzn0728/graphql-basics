239e93ba41df4db76a40cda8aed0ca985607bc82
Chandler Fang <thames.ft@gmail.com>
2020-05-29 15:18:54 -0400

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+
+
+import myCurrentLocation, { getGreeting, message, name } from './myModule'
+
+import substractFunction, {add} from './math'
+
+console.log(message, name)
+console.log(myCurrentLocation)
+console.log(getGreeting('Jessica'))
+
+console.log(add(2,5))
+console.log(substractFunction(2,7))
---
+++ src/math.js
@@ -0,0 +1,14 @@
+const add = (a,b) => {
+
+    return a+b
+}
+
+const substract = (c, d) =>{
+
+    return c-d
+}
+
+export {
+    add,
+    substract as default
+}
