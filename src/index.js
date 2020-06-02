b947412f7b36bfafef866056fb98b6596df09eaf
Chandler Fang <thames.ft@gmail.com>
2020-06-02 13:42:36 -0400

    Regular update
    Add operation function, function feature in graphql, add nodemon

--- src/index.js
+++ src/index.js
@@ -1,12 +1,76 @@
+import {GraphQLServer} from 'graphql-yoga'
+
+// String, Boolean, Int, Float, ID 
 
 
-import myCurrentLocation, { getGreeting, message, name } from './myModule'
 
-import substractFunction, {add} from './math'
+// Type definition (schema)
+const typeDefs = `
+    type Query {
+        greeting(name: String, position: String): String!
+        add(a: Float!, b: Float!): Float!
+        me: User!
+        post: Post!
+    }
 
-console.log(message, name)
-console.log(myCurrentLocation)
-console.log(getGreeting('Jessica'))
+    type User {
+        id: ID!
+        name: String!
+        email: String!
+        age: Int
+    }
 
-console.log(add(2,5))
-console.log(substractFunction(2,7))
+    type Post {
+        id: ID!
+        title: String!
+        body: String!
+        published: Boolean!
+    }
+`
+
+// Resolvers
+const resolvers = {
+    Query: {
+        greeting(parent, args, ctx, info){
+            if (args.name && args.position){
+                return `Hello, ${args.name} and my favorite ${args.position}!`
+            } else{
+                return `Hello!`
+        } 
+        },
+
+        add(parent, args, ctx, info){
+            return args.a + args.b
+        },
+
+        me() {
+            return {
+                id: '123456',
+                name: 'Chandler',
+                email: 'xxxemail',
+
+
+            }
+        },
+
+        post() {
+            return {
+                id: '213456',
+                title: 'Story1',
+                body: 'Here is the main body',
+                published: true
+            }
+        }
+        
+    }
+
+}
+
+const server = new GraphQLServer({
+    typeDefs,
+    resolvers
+})
+
+server.start(() => {
+    console.log('The server is up!')
+}) 
